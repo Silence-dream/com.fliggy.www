b8d5d3ef9646f19d8ff2d52f8922a841e2f0219a
Silence-dream <1428482231@qq.com>
2020-01-03 21:03:09 +0800

    提交：2020.1.3代码 商店和签到页面

--- js/index.js
+++ js/index.js
@@ -19,3 +19,71 @@ user.onclick = function () {
 }
 
 
+
+
+
+
+/* 鼠标滚动事件 */
+let topEntirety = document.getElementById("top-entirety");
+
+// window.onmousewheel = function () {
+//     topEntirety.style.position = "fixed"
+//     topEntirety.style.top = "0";
+//     topEntirety.style.left = "0";
+//     topEntirety.style.zIndex = "999";
+//     topEntirety.style.backgroundColor = "#ff3b32";
+//     topEntirety.style.width = "100%"
+//     console.log(1)
+// }
+// window.onscroll = function () {
+//     console.log(2)
+// }
+
+
+var agent = navigator.userAgent;
+if (/.*Firefox.*/.test(agent)) {
+    document.addEventListener("DOMMouseScroll", function (e) {
+        e = e || window.event;
+        var detail = e.detail;
+        if (detail > 0) {
+            // console.log("鼠标向下滚动");
+            topEntirety.style.position = "fixed"
+            topEntirety.style.top = "0";
+            topEntirety.style.left = "0";
+            topEntirety.style.zIndex = "999";
+            topEntirety.style.backgroundColor = "#ff3b32";
+            topEntirety.style.width = "100%"
+            // topEntirety.style.transitionProperty = "all"
+            // topEntirety.style.transitionDuration = "0.4s"
+
+        } else {
+            // console.warn("鼠标向上滚动");
+            topEntirety.style.cssText = "";
+            // topEntirety.style.position = "static";
+            // topEntirety.style.backgroundColor = "blue";
+        }
+    });
+}
+else {
+    document.onmousewheel = function (e) {
+        e = e || window.event;
+        var wheelDelta = e.wheelDelta;
+        if (wheelDelta > 0) {
+            topEntirety.style.cssText = "";
+            // topEntirety.style.position = "static"
+            // console.log("鼠标向上滚动");
+
+        } else {
+            topEntirety.style.position = "fixed"
+            topEntirety.style.top = "0";
+            topEntirety.style.left = "0";
+            topEntirety.style.zIndex = "999";
+            topEntirety.style.backgroundColor = "#ff3b32";
+            topEntirety.style.width = "100%"
+            // console.warn("鼠标向下滚动");
+        }
+    }
+}
+
+
+
